c738c7c94c9c70c341c3497e9526db9d21d97aa9
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-17 18:49:48 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,5 +1,21 @@
-Explore different data types in JavaScript: Number, String, Boolean, Null, Undefined, Object, Symbol
-
-Writo scripts to demonstrate typo conversion (String(), Number(), implicit conversion)
-
-Try examples of converting froin a string to a number and vice versa
+console.log(String(123));
+console.log(String(true));
+console.log(String(null));
+console.log(String(undefined));
+console.log(Number("123"));
+console.log(Number("abc"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(Number(null)); 
+console.log(Number(undefined)); 
+console.log("5" - 3);
+console.log("5" + 3); 
+console.log(true + 1);
+let str = "123";
+let num = Number(str);
+console.log(num); 
+console.log(typeof num);
+let num = 123;
+let str = String(num);
+console.log(str);
+console.log(typeof str); 
