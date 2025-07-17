cb656b43739c336f995aaa1b6ab55ad103da2dae
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-17 18:50:55 +0530

    Day1 Javascript

--- Task2.js
+++ Task2.js
@@ -2,19 +2,23 @@ console.log(String(123));
 console.log(String(true));
 console.log(String(null));
 console.log(String(undefined));
+
 console.log(Number("123"));
 console.log(Number("abc"));
 console.log(Number(true));
 console.log(Number(false));
 console.log(Number(null)); 
 console.log(Number(undefined)); 
+
 console.log("5" - 3);
 console.log("5" + 3); 
 console.log(true + 1);
+
 let str = "123";
 let num = Number(str);
 console.log(num); 
 console.log(typeof num);
+
 let num = 123;
 let str = String(num);
 console.log(str);
