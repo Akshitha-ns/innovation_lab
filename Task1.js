bd8050e7159c338255f434d4b010944869c81f31
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-17 18:23:55 +0530

    Day1 Javascript

---
+++ Task1.js
@@ -0,0 +1,21 @@
+console.log("--- Using 'var' ---");
+
+var greeting = "Hello";
+console.log("Initial greeting (var):", greeting); 
+var greeting = "Hi there";
+console.log("Re-declared greeting (var):", greeting); 
+greeting = "Good morning";
+console.log("Re-assigned greeting (var):", greeting); 
+
+console.log("--- Using 'let' ---");
+
+let count = 10;
+console.log("Initial count (let):", count); 
+count = 20;
+console.log("Re-assigned count (let):", count); 
+
+console.log("\n--- Using 'const' ---");
+
+const PI = 3.14159;
+console.log("Value of PI (const):", PI);
+
