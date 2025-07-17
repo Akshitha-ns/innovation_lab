d3133f0caa1b261072e814b5bb15dbdd33715883
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-17 18:37:53 +0530

    Update Task2.js

--- Task2.js
+++ Task2.js
@@ -1,1 +1,5 @@
+Explore different data types in JavaScript: Number, String, Boolean, Null, Undefined, Object, Symbol
 
+Writo scripts to demonstrate typo conversion (String(), Number(), implicit conversion)
+
+Try examples of converting froin a string to a number and vice versa
