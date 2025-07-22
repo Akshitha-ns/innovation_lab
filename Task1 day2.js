1dc051e2c173875b992fc6d32a6808c8184ccd96
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-22 18:09:15 +0530

    Day2  Javascript

---
+++ Task1 day2.js
@@ -0,0 +1,51 @@
+// if statement 
+
+grade ='A';
+if grade=='A':
+    print("Excellent!");
+print("This message always prints as default");
+
+//if-else block
+
+age = 17;
+if age >= 18:
+    print("Eligible for voting");
+else:
+    print("Not Eligible for voting");
+print("Age check complete.");
+
+// Multiple else if statements
+
+score = 86;
+if score >= 90:
+    print("Grade: A")
+else if score >= 80:
+    print("Grade: B")
+else if score >= 70:
+    print("Grade: C")
+else if score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+print("Grading complete.")
+
+// switch using if-elif-else
+
+day_number = 3 
+if day_number == 1:
+    print("It's Sunday!")
+elif day_number == 2:
+    print("It's Monday!")
+elif day_number == 3:
+    print("It's Tuesday!")
+elif day_number == 4:
+    print("It's Wednesday!")
+elif day_number == 5:
+    print("It's Thursday!")
+elif day_number == 6:
+    print("It's Friday!")
+elif day_number == 7:
+    print("It's Saturday!")
+else:
+    print("Invalid day number. Please enter a number between 1 and 7.")
+
