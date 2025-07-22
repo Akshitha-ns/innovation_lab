96a08dd3e31656099cb08c937fd51bf0063329c4
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-22 19:04:18 +0530

    Day2  Javascript

--- day2_project.js
+++ day2_project.js
@@ -2,9 +2,40 @@ function display(){
   console.log("simple calculator");
   console.log("1. Add");
   console.log("2. subtract");
-  console.log(" 3. multiply");
+  console.log("3. multiply");
   console.log("4. Divide");
-  console.log("5. Exit");
   choice = prompt("Enter your choice: ");
   return choice;
 }
+
+function calculator() {
+  let num1,num2,result;
+  let choice = display();
+
+  switch (choice) {
+    case 1:
+      result = num1 + num2;
+      break;
+    case 2 :
+      result = num1 - num2;
+      break;
+    case 3:
+      result = num1 * num2;
+      break;
+    case 4:
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        console.log("Error: Division by zero is not allowed.");
+        return;
+      }
+      break;
+    default:
+      console.log("Error: Invalid choice .");
+      return;
+  }
+
+  alert(`The result is: ${result}`);
+}
+
+calculator();
