f1fb8c8256e404bc9605114a7a3ef519757ba1a7
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-22 18:36:10 +0530

    Day2  Javascript

--- Task1 day2.js
+++ Task1 day2.js
@@ -1,51 +1,66 @@
 // if statement 
 
 grade ='A';
-if grade=='A':
-    print("Excellent!");
-print("This message always prints as default");
+if grade=='A'{
+    console.log("Excellent!");
+console.log("This message always prints as default");
+}
 
 //if-else block
 
 age = 17;
-if age >= 18:
-    print("Eligible for voting");
-else:
-    print("Not Eligible for voting");
-print("Age check complete.");
+if age >= 18{
+    console.log("Eligible for voting");
+}
+else{
+    console.log("Not Eligible for voting");
+console.log("Age check complete.");
+}
 
 // Multiple else if statements
 
 score = 86;
-if score >= 90:
-    print("Grade: A")
-else if score >= 80:
-    print("Grade: B")
-else if score >= 70:
-    print("Grade: C")
-else if score >= 60:
-    print("Grade: D")
-else:
-    print("Grade: F")
-print("Grading complete.")
+if score >= 90{
+    console.log("Grade: A")
+}
+else if score >= 80{
+    console.log("Grade: B")
+}
+else if score >= 70{
+    console.log("Grade: C")
+}
+else if score >= 60{
+    console.log("Grade: D")
+}
+else{
+    console.log("Grade: F")
+}
+console.log("Grading complete.")
 
 // switch using if-elif-else
 
 day_number = 3 
-if day_number == 1:
-    print("It's Sunday!")
-elif day_number == 2:
-    print("It's Monday!")
-elif day_number == 3:
-    print("It's Tuesday!")
-elif day_number == 4:
-    print("It's Wednesday!")
-elif day_number == 5:
-    print("It's Thursday!")
-elif day_number == 6:
-    print("It's Friday!")
-elif day_number == 7:
-    print("It's Saturday!")
-else:
-    print("Invalid day number. Please enter a number between 1 and 7.")
-
+if day_number == 1{
+    console.log("It's Sunday!")
+}
+else if day_number == 2{
+    console.log("It's Monday!")
+}
+else if day_number == 3{
+    console.log("It's Tuesday!")
+}
+else if day_number == 4{
+    console.log("It's Wednesday!")
+}
+else if day_number == 5{
+    console.log("It's Thursday!")
+}
+else if day_number == 6{
+    console.log("It's Friday!")
+}
+else if day_number == 7{
+    console.log("It's Saturday!")
+}
+else{
+    console.log("Invalid day number. Please enter a number between 1 and 7.")
+}
