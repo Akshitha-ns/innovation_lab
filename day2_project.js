7897f74fe844e8d2964a5075fbbed6ca64a2b541
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-22 18:59:57 +0530

    Create day2_project.js

---
+++ day2_project.js
@@ -0,0 +1,10 @@
+function display(){
+  console.log("simple calculator");
+  console.log("1. Add");
+  console.log("2. subtract");
+  console.log(" 3. multiply");
+  console.log("4. Divide");
+  console.log("5. Exit");
+  choice = prompt("Enter your choice: ");
+  return choice;
+}
