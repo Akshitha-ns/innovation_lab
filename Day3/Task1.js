3da5424870333a6d77dba4fba88515cda63ff6dd
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-23 19:47:29 +0530

    Day3 javascript

--- Day3/Task1.js
+++ Day3/Task1.js
@@ -1,1 +1,16 @@
-b
+function greeting(name) {
+    console.log("Hello ",name);
+}
+
+greeting("Riya"); 
+
+
+function Addition() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    console.log(sum);
+}
+
+Addition(1, 2, 3, 4, 5);
