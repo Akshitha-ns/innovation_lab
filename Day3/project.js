833c3c5c7ac54242c4b2437b828c3cdae67b360d
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-07-24 18:29:53 +0530

    Day3 javascript

--- Day3/project.js
+++ Day3/project.js
@@ -1,4 +1,3 @@
-// Task manager module
 let tasks = [];
 
 const addTask = (task, callback) => {
@@ -10,25 +9,20 @@ const printTasks = () => {
     console.log("Current tasks:");
     tasks.forEach((task, index) => console.log(`${index + 1}. ${task}`));
 };
-
-// Demonstrate use of arbitrary arguments
 const addMultipleTasks = (...tasksToAdd) => {
     tasks.push(...tasksToAdd);
     console.log(`Added ${tasksToAdd.length} tasks.`);
 };
 
-// Illustrate variable scopes
 let globalVar = "I'm global";
 function scopeExample() {
     let localVar = "I'm local";
-    console.log(globalVar); // Accessible
-    console.log(localVar);   // Accessible
+    console.log(globalVar); 
+    console.log(localVar);  
 }
 scopeExample();
-console.log(globalVar); // Accessible
-// console.log(localVar); // Not accessible (ReferenceError)
+console.log(globalVar); 
 
-// Example usage
 addTask("Buy groceries", () => console.log("Task added: Buy groceries"));
 addTask("Do laundry", printTasks);
 addMultipleTasks("Clean room", "Study JavaScript", "Exercise");
