09b3a0abd09a5cd85ddec1a4a0d5e7c61b4783c8
Akshitha-ns <akshithanagaraj16@gmail.com>
2025-08-03 13:15:45 +0530

    object creation

--- day4/task2.js
+++ day4/task2.js
@@ -1,17 +1,12 @@
-// Create objects representing real-world entities
 const student = {
   name: 'John Doe',
   age: 20,
-  // Add a method to display info
   displayInfo: function() {
     console.log(`Name: ${this.name}, Age: ${this.age}`);
   }
 };
 
-// Invoke the method
-student.displayInfo(); // Output: "Name: John Doe, Age: 20"
-
-// Accessing and modifying object properties
-console.log(student.name); // Output: "John Doe"
+student.displayInfo();
+console.log(student.name); 
 student.age = 21;
-console.log(student.age); // Output: 21
+console.log(student.age);
